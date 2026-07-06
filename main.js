19271df40d4dd381b2c4117cd4b665d70162f6ff
stewie <class35123@gmail.com>
2026-07-06 16:48:40 +0700

    6.7 add toggle stock button

--- main.js
+++ main.js
@@ -9,16 +9,33 @@ createApp({
     const inStock = ref(true)
     const inventory = ref(15)
     const onSale = ref(true)
+
     const details = ref([
       '50% cotton',
       '30% wool',
       '20% polyester'
     ])
+
     const variants = ref([
-      { id: 2234, color: 'green' },
-      { id: 2235, color: 'blue' }
+      { id: 2234, color: 'green', image:'./assets/images/socks_green.jpg' },
+      { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg' }
     ])
+
     const sizes = ref(['S', 'M', 'L'])
+    const cart = ref(0)
+
+    function addToCart(){
+      cart.value += 1
+    }
+
+    function updateImage(variantImage){
+      image.value = variantImage
+    }
+
+    function toggleInStock(){
+      inStock.value = !inStock.value
+    }
+
     return {
       product,
       image,
@@ -29,7 +46,11 @@ createApp({
       onSale,
       details,
       variants,
-      sizes
+      sizes,
+      cart,
+      addToCart,
+      updateImage,
+      toggleInStock
     }
   }
 
