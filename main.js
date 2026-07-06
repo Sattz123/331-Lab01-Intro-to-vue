1a897e370530bb9fa867a5c5e14b1df8139a73e9
stewie <class35123@gmail.com>
2026-07-06 16:13:33 +0700

    4.9 add onSale

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ createApp({
     const image = ref('./assets/images/socks_green.jpg')
     const productDescription = ref('A pair of warm, fuzzy boots')
     const url = ref('https://www.camt.cmu.ac.th')
+    const inStock = ref(true)
+    const inventory = ref(15)
+    const onSale = ref(true)
     return {
       product,
       image,
       productDescription,
-      url
+      url,
+      inStock,
+      inventory,
+      onSale
     }
   }
 
